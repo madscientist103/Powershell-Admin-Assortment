ecf4f7f861dd835b263b11e102f717f254b246a2
Clayton Carter <72357247+madscientist103@users.noreply.github.com>
2020-10-30 23:51:57 -0400

    Add files via upload
    auto_w10_inplace_upgrade -  automate launch for workstation w10 in-place upgrade

---
+++ auto_w10_inplace_upgrade.ps1
@@ -0,0 +1,5 @@
+﻿net use e: \\path\to\windows\iso
+Mount-DiskImage -ImagePath "e:\windows.iso"
+set-variable drive -value (Get-DiskImage "E:\windows.iso" | Get-Volume).DriveLetter
+cd ${drive}:
+cmd setup.exe /auto upgrade /quiet
